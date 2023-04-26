793674fd8645d826470c9101940c46613c96e673
denis.akimov <denis.akimov@codemark.ru>
2023-04-26 18:19:13 +0600

    ROCKET-44 Поиск не ищет

--- apps/meteor/server/startup/migrations/v293.ts
+++ apps/meteor/server/startup/migrations/v293.ts
@@ -1,9 +1,20 @@
 import { addMigration } from '../../lib/migrations';
+import { MongoInternals } from 'meteor/mongo';
 import { upsertPermissions } from '../../../app/authorization/server/functions/upsertPermissions';
 
 addMigration({
 	version: 293,
+	name: 'Add index on message attachment description',
 	async up() {
+		const { mongo } = MongoInternals.defaultRemoteCollectionDriver();
+		const messages = mongo.db.collection('rocketchat_message');
+
+		await messages.dropIndex('msg_text')
+
+		await messages.createIndex({
+			'attachments.description': 'text',
+			msg: 'text'
+		})
 		await upsertPermissions();
-	},
-});
+	}
+})
