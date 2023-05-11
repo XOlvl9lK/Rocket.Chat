b46f73e5b3cdbdd585557c0c5aaba83c8b2e2429
akimov.denis <akimov.denis@gost-group.com>
2023-05-11 19:00:58 +0600

    ROCKET-49 Поиск по файлам вложений

--- apps/meteor/server/startup/migrations/v295.ts
+++ apps/meteor/server/startup/migrations/v295.ts
@@ -1,46 +1,9 @@
 import { addMigration } from '../../lib/migrations';
-import { MongoInternals } from 'meteor/mongo';
-import { getContentParser } from '/app/api/server/lib/contentParser';
-import { FileUpload } from '/app/file-upload/server';
-import type { IUpload } from '@rocket.chat/core-typings';
-import { Uploads } from '@rocket.chat/models';
-
-const getFileBufferPromise = (file: IUpload) => {
-	return new Promise<Buffer>((resolve, reject) => {
-		FileUpload.getBuffer(file, (e, data) => {
-			if (e) reject(e)
-			resolve(data)
-		})
-	})
-}
 
 addMigration({
 	version: 295,
 	name: 'Reindex uploaded files',
 	async up() {
-		const { mongo } = MongoInternals.defaultRemoteCollectionDriver();
-		const messageModel = mongo.db.collection('rocketchat_message');
-
-		const messages = await messageModel.find({ attachments: { $elemMatch: { type: 'file' }}}).toArray()
-
-		for (let i = 0; i < messages.length; i++) {
-			const message = messages[i]
-			const attachments = message.attachments
-			for (let j = 0; j < attachments; j++) {
-				const attachment = attachments[j]
-				const titleLink = attachment?.title_link?.replace('/file-upload/', '')
-				const match = /^\/([^\/]+)\/(.*)/.exec(titleLink || '');
-				if (match?.[1]) {
-					const file = await Uploads.findOneById(match[1])
-					if (file) {
-						const fileBuffer = await getFileBufferPromise(file)
-						if (fileBuffer) {
-							attachment.fileContent = await getContentParser(file.type, fileBuffer).parse()
-							await messageModel.updateOne({ _id: message._id }, { $set: { attachments }})
-						}
-					}
-				}
-			}
-		}
+		// noop
 	}
 })
---
+++ apps/meteor/server/startup/migrations/v297.ts
@@ -0,0 +1,50 @@
+import { addMigration } from '../../lib/migrations';
+import { MongoInternals } from 'meteor/mongo';
+import { getContentParser } from '/app/api/server/lib/contentParser';
+import { FileUpload } from '/app/file-upload/server';
+import type { IUpload } from '@rocket.chat/core-typings';
+import { Uploads } from '@rocket.chat/models';
+
+const getFileBufferPromise = (file: IUpload) => {
+	return new Promise<Buffer>((resolve, reject) => {
+		FileUpload.getBuffer(file, (e, data) => {
+			if (e) reject(e)
+			resolve(data)
+		})
+	})
+}
+
+addMigration({
+	version: 297,
+	name: 'Reindex uploaded files',
+	async up() {
+		const { mongo } = MongoInternals.defaultRemoteCollectionDriver();
+		const messageModel = mongo.db.collection('rocketchat_message');
+
+		const messages = await messageModel.find({ attachments: { $elemMatch: { type: 'file' }}}).toArray()
+
+		console.log('start reindex messages');
+		for (let i = 0; i < messages.length; i++) {
+			const message = messages[i]
+			const attachments = message.attachments
+			for (let j = 0; j < attachments; j++) {
+				const attachment = attachments[j]
+				const titleLink = attachment?.title_link?.replace('/file-upload', '')
+				console.log('titleLink', titleLink);
+				const match = /^\/([^\/]+)\/(.*)/.exec(titleLink || '');
+				if (match?.[1]) {
+					const file = await Uploads.findOneById(match[1])
+					console.log('file id', file?._id);
+					if (file) {
+						const fileBuffer = await getFileBufferPromise(file)
+						if (fileBuffer) {
+							console.log('is buffer found', !!fileBuffer);
+							attachment.fileContent = await getContentParser(file.type, fileBuffer).parse()
+							await messageModel.updateOne({ _id: message._id }, { $set: { attachments }})
+						}
+					}
+				}
+			}
+		}
+	}
+})
