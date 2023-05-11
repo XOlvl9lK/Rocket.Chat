4897b9ae5eb9907a788188a6f710399b4b0f1725
akimov.denis <akimov.denis@gost-group.com>
2023-05-11 16:56:30 +0600

    ROCKET-49 Поиск по файлам вложений

---
+++ apps/meteor/server/startup/migrations/v296.ts
@@ -0,0 +1,10 @@
+import { addMigration } from '../../lib/migrations';
+import { upsertPermissions } from '../../../app/authorization/server/functions/upsertPermissions';
+
+addMigration({
+	version: 293,
+	name: 'Add index on message attachment description',
+	async up() {
+		await upsertPermissions();
+	}
+})
